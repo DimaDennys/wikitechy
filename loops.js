4f7fb1d157d86cb313720f529a1df6d72a6201b0
DimaDennys <dumitrudenisiuc@gmail.com>
2020-06-22 15:59:44 +0100

    array.forEach() loop

--- loops.js
+++ loops.js
@@ -5,6 +5,7 @@ for(i = 0; i <= 5; i++){
     console.log("This is a for loop, x = ", x)
 }
 
+//---
 x= 0;
 
 while (x!=5){
@@ -12,10 +13,20 @@ while (x!=5){
     console.log("This is a WHILE loop, x = ", x)
 }
 
+//---
 x= 0;
 
 do{
     x++;
     console.log("This is a DO{}WHILE(cond) loop, x = ", x)
 } while(x!=4);
+
+//---
+const a = [1,2,3,4,5,6]
+
+a.forEach(x => {
+    console.log('forEach loop, Value of x is', x)
+})
+
+//---
 
