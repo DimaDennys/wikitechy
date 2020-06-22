8aa70ccbbb4c7026e6d67e06513f4249c052f0a4
DimaDennys <dumitrudenisiuc@gmail.com>
2020-06-22 17:58:40 +0100

    event loop vertion 2

--- eventloop.js
+++ eventloop.js
@@ -1,17 +1,40 @@
+// const events = require('events');
+// const myEvents = events.EventEmitter;
+// const myLogger = new myEvents;
+
+// myLogger.on('err', errLogger);
+// myLogger.on('error', errorLogger);
+
+// myLogger.emit('err', 'Error 1');
+// myLogger.emit('error', 'Error 2');
+
+// function errLogger(message) {
+//     console.log('err ', message);
+// }
+
+// function errorLogger(message) {
+//     console.log(message);
+// }
+
+// version 2
+//import events module 
 const events = require('events');
-const myEvents = events.EventEmitter;
-const myLogger = new myEvents;
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+//Create an event handler for connnectHandler
+eventEmitter.on('connection', connnectHandler);
 
-myLogger.on('err', errLogger);
-myLogger.on('error', errorLogger);
-
-myLogger.emit('err', 'Error 1');
-myLogger.emit('error', 'Error 2');
-
-function errLogger(message) {
-    console.log('err ', message);
+// Bind the data event
+const dataReceivedHandler = function (){
+    console.log('Data received');
 }
 
-function errorLogger(message) {
-    console.log(message);
+// Create an event handler for dataReceivedHandler
+eventEmitter.on('data_received', dataReceivedHandler);
+// Fire the connection event
+eventEmitter.emit('connection');
+eventEmitter.emit('data_received');
+// Bind the connection event
+function connnectHandler(){
+    console.log('Connection established');
 }
