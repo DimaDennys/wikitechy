0c8f93540a0de989c03a05df81e33631069d0a6f
DimaDennys <dumitrudenisiuc@gmail.com>
2020-06-24 12:45:08 +0100

    two more routes for node and angular

--- express/index.js
+++ express/index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express();
+
+app.route('/node').get((req, res) => {
+    res.send('This route for Node')
+})
+
+app.route('/angular').get((req, res) => {
+    res.send('This route for Angular');
+})
+
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
